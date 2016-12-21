b46f4ccb79aaff4a4ce0f91163dea386f11670c2
Aman Dureja <dureja.aman.97@gmail.com>
2016-12-21 02:35:19 -0500

    int to binary encoding

--- binary_operations.ml
+++ binary_operations.ml
@@ -1,4 +1,4 @@
-(* Functions for operations on binary numbers *)
+(* Functions for operations on binary numbers represented as arrays of booleans *)
 
 let twos_compl bin =
   let complement = Array.copy bin in
@@ -21,3 +21,19 @@ let int_of_binary_unsigned bin =
 let int_of_binary_signed bin =
   if not bin.(0) then int_of_binary_unsigned bin
   else -1 * int_of_binary_unsigned (twos_compl bin);;
+
+let rec binary_of_int n =
+  if n < 0 then twos_compl (binary_of_int (n * -1))
+  else begin
+    let num = ref n in
+    let bin = Array.make 32 false in
+    for i = (Array.length bin) - 1 downto 0 do
+      if (!num < 0) then bin.(i) <- false
+      else begin
+        match !num mod 2 with
+          | 0 -> bin.(i) <- false; num := !num / 2;
+          | 1 -> bin.(i) <- true; num := !num / 2;
+      end;
+    done;
+    bin;
+  end;;
--- cpu.ml
+++ cpu.ml
@@ -10,42 +10,42 @@ open Binary_operations;;
 class cpu =
     object (self)
       (* General Setup *)
-      val minReg = 0
-      val maxReg = 15
-      val pc = 15
-      val stackPointer = 13
-      val linkRegister = 14
-      val mutable generalRegisters : bool array array = Array.make 32 [||]
-      val mutable memory : bool array array = Array.make 2048 [||]
+      val minReg = 0;
+      val maxReg = 15;
+      val pc = 15;
+      val stackPointer = 13;
+      val linkRegister = 14;
+      val mutable generalRegisters : bool array array = Array.make 32 [||];
+      val mutable memory : bool array array = Array.make 2048 [||];
       (* Interstage Registers *)
-      val mutable rA : bool array = Array.make 32 false
-      val mutable rB : bool array = Array.make 32 false
-      val mutable rM : bool array = Array.make 32 false
-      val mutable rZ : bool array = Array.make 32 false
-      val mutable rY : bool array = Array.make 32 false
+      val mutable rA : bool array = Array.make 32 false;
+      val mutable rB : bool array = Array.make 32 false;
+      val mutable rM : bool array = Array.make 32 false;
+      val mutable rZ : bool array = Array.make 32 false;
+      val mutable rY : bool array = Array.make 32 false;
       (* Instruction Register *)
-      val mutable ir : bool array = Array.make 16 false
+      val mutable ir : bool array = Array.make 16 false;
       (* Methods and Instruction Execution Stages *)
       method validateRegNumber n =
-        if n < 0 || n > 31 then failwith "Invalid register number!"
+        if n < 0 || n > 31 then failwith "Invalid register number!";
       method printState = ()
       method loadProgramInMem = ()
       method aluOp command =
         match command with
           | "plus" -> ()
           | "minus" -> ()
-          | _ -> failwith "Invalid ALU command!"
+          | _ -> failwith "Invalid ALU command!";
       method fetch =
-        ir = Array.copy memory.(0);
-        self#decode
+        ir = Array.append memory.(int_of_binary_unsigned generalRegisters.(pc)) memory.(int_of_binary_unsigned generalRegisters.(pc + 1));
+        self#decode;
       method decode =
 
-        self#execute
+        self#execute;
       method execute =
 
-        self#memory
+        self#memory;
       method memory =
 
-        self#writeback
+        self#writeback;
       method writeback = ()
-    end
+    end;;
